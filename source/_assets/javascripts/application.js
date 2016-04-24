306e909b69d649456c29dfc7245e9b0fe29c44d8
kylepfeeley <kylepfeeley@gmail.com>
2016-04-24 17:31:35 -0400

    Adding Tooltips

--- source/_assets/javascripts/application.js
+++ source/_assets/javascripts/application.js
@@ -1,4 +1,6 @@
 $(function() {
+	// Scroll to assigned section on button click
+
 	$('a[href^="#"]').on('click', function(event) {
 
 	    var target = $( $(this).attr('href') );
@@ -10,5 +12,13 @@ $(function() {
 	        }, 1000);
 	    }
 
+	});
+
+	// Tooltip on hover
+
+	var $info = $('[class^="icon"]');
+	$info.each( function () {
+	  var dataInfo = $(this).data("tooltip");
+	  $( this ).append('<span class="inner" >' + dataInfo + '</span>');
 	});
 });
